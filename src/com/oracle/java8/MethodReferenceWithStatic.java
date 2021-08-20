2fb4b59d4793ec0c5e0dc65b36639184c297eaac
sathiayaraj45 <sathiyaraj45@gmail.com>
2021-08-20 20:02:18 +0400

    multiple interface created

--- src/com/oracle/java8/MethodReferenceWithStatic.java
+++ src/com/oracle/java8/MethodReferenceWithStatic.java
@@ -17,10 +17,20 @@ interface HomeInterface {
 	void kitchen();
 }
 
+interface HomeInterfaceSecond {
+	int kitchenRoomSize(int roomLength, int roomWidth);
+}
+
 public class MethodReferenceWithStatic {
 	//Personal kitchen service 
 	public static void myKitchen() {
 		System.out.println("It's my new kitchen design by David");
+	}
+	
+	// Calculate kitchen area square feet
+	public static int areaOfKitchen(int l, int w)
+	{
+		return l * w;
 	}
 	
 
@@ -31,10 +41,14 @@ public class MethodReferenceWithStatic {
 		// TODO Auto-generated method stub
 		// My personal kitchen mapping to home kitchen interface using static method reference 
 		HomeInterface home = MethodReferenceWithStatic::myKitchen;
+		HomeInterfaceSecond homeTwo = MethodReferenceWithStatic::areaOfKitchen;
 		
 		//Now call the interface kitchen
 		home.kitchen();
 		
+		//Area of Home
+		int areaOfRoomSize = homeTwo.kitchenRoomSize(10, 10);
+		System.out.println("Room Square feet ==> "+ areaOfRoomSize + "sqrt");
 	}
 
 }
