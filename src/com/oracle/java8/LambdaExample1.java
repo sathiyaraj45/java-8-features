09cb5e9e63803d353ea074d4f518cad829272ea3
sathiayaraj45 <sathiyaraj45@gmail.com>
2022-02-06 21:16:13 +0400

    lambda example added

---
+++ src/com/oracle/java8/LambdaExample1.java
@@ -0,0 +1,25 @@
+package com.oracle.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Sathiyaraj created on 06-02-2022
+ * <p>
+ * TODO
+ */
+
+
+public class LambdaExample1 {
+    public static void main(String  args[]) {
+        List<String> fruits = new ArrayList<String>();
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("mango");
+        fruits.add("orange");
+
+        fruits.stream().forEach(obj -> {
+            System.out.println("Fruit name is =>"+ obj);
+        });
+    }
+}
